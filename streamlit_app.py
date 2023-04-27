3cf39f3fe0816952cacdf559079bae3772e1ba48
monybt <131964812+monybt@users.noreply.github.com>
2023-04-27 15:55:30 +0530

    Update streamlit_app.py
    Added one more item to breakfast list

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,8 @@
 import streamlit
 streamlit.title('My Parents new Healthy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('kale ,Spinach & Rocket Smooethie')
-streamlit.text('Hard-Boiled, Free-Range Egg')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 kale ,Spinach & Rocket Smooethie')
+streamlit.text('🐔 Hard-Boiled, Free-Range Egg')
+streamlit.text('🥑🍞 Avacado Toast')
+  
