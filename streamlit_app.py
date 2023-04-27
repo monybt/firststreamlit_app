405964b2ab0e1565b008e81f38b5c89080781321
monybt <131964812+monybt@users.noreply.github.com>
2023-04-27 15:51:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 
 import streamlit
 streamlit.title('My Parents new Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('kale ,Spinach & Rocket Smooethie')
+streamlit.text('Hard-Boiled Free-Range Egg')
