b36eff7fc420e65f77ff68d887f6c010af1a3b50
monybt <131964812+monybt@users.noreply.github.com>
2023-05-02 16:36:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,6 +26,7 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json()) #writes the data to the screen
+# take the json version and normalize
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# output it on screen
+streamlit.dataframe(fruityvice_normalized)
