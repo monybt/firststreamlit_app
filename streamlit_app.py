c0480c37ccd5508becc0195e6038ea9040ebc5e2
monybt <131964812+monybt@users.noreply.github.com>
2023-05-02 18:20:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,4 +44,4 @@ streamlit.dataframe(my_data_rows)
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','Kiwi')
 add_my_fruit_response = requests.get("https://fruityvice.com/api/fruit/" + add_my_fruit)
-streamlit.write('Thanks for adding ', fruit_choice)
+streamlit.write('Thanks for adding ', add_my_fruit)
